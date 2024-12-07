a213da4a640d1b94c7b4a4118ef04a830d120538
MATHEUS VIEIRA <matheus.vieira137@etec.sp.gov.br>
2024-12-06 21:09:13 -0300

    Feat: Adicionado codigo que consome a API

---
+++ WeatherAPI/WeatherAPI/Models/WeatherResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WeatherAPI.Models
+{
+    public class WeatherResponse
+    {
+        [JsonPropertyName("temp")]
+        public string Temperature;
+
+        [JsonPropertyName("humidity")]
+        public string Humidity;
+
+        [JsonPropertyName("wind_speed")]
+        public string Wind;
+
+        [JsonPropertyName("sunrise")]
+        public string Sunrise;
+
+        [JsonPropertyName("sunset")]
+        public string Sunset;
+
+        public List<WeatherModel> WeatherModel { get; set; }
+    }
+}
---
+++ WeatherAPI/WeatherAPI/Services/MainService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using WeatherAPI.Models;
+
+namespace WeatherAPI.Services
+{
+    public class MainService
+    {
+        private HttpClient _httpClient;
+        private string apiKey = "";
+        private WeatherResponse weatherResponse;
+        private JsonSerializerOptions jsonSerializerOptions;
+
+        public MainService() 
+        {
+            _httpClient = new HttpClient();
+            jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<WeatherResponse> GetWeatherByCity(string cityName)
+        {
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}";
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    weatherResponse = JsonSerializer.Deserialize<WeatherResponse>(jsonResponse, jsonSerializerOptions);
+                    if (weatherResponse == null)
+                    {
+                        Console.WriteLine("Erro na deserialização da resposta da API: o objeto retornado é nulo.");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Erro ao obter os dados do clima. ERRO: " + response.StatusCode);
+                }
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Falha na requisição da API.", ex);
+            }
+            return weatherResponse;
+        }
+    }
+}
